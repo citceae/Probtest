947d53b9342d3aa8266f0377edfbf927e52635b0
citceae <ztye@pku.edu.cn>
2021-03-12 00:14:39 +0800

    control part:sy.py

---
+++ test/gcovtest/sy.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import random
+import bisect
+
+#python3 sy.py func.c sup inf
+#loop until ? several times
+
+def randinitcase(start, stop, length):
+    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
+    length = int(abs(length)) if length else 0
+    random_list = []
+    for i in range(length):
+        random_list.append(random.randint(start, stop))
+    return random_list
+        
+
+def dividepart(reward:list,partition:list):
+    if len(reward) == 0 and len(partition) == 2:
+        start = partition[0]
+        stop = partition[1]
+        mid = int((stop + start)/2)
+        partition.append(mid)
+        partition.sort()
+    else:# TODO fix when reward is got
+        return
+
+def dividecases(allcases:list, cases:list, partition:list):
+    for case in allcases:
+        idx = bisect.bisect(partition,case)-1
+        if idx < 0:
+            print("idx error")
+        while idx > len(cases)-1:
+            cases.append([])
+        cases[idx].append(case)
+    
+
+def recpile(cmdarg):
+    cmdstr = "clang {filename} -emit-llvm -c -o test.bc -g && gcc -fprofile-arcs -ftest-coverage {filename} -o test".format(filename = cmdarg)
+    print(cmdstr)
+    os.system(cmdstr)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 3:
+        print('''error''')
+        exit()
+
+    partition=[]
+    partition.append(int(args[2]))
+    partition.append(int(args[3]))
+    #rand sample for k=100 times
+    allcases = randinitcase(args[2],args[3],100) #all cases
+    #print(allcases)
+
+    #init
+    reward = [] #reward of different partition
+    cases = [[]]
+
+    #choose a partition and divide cases into different partition (half)
+    dividepart(reward,partition)
+    #print(partition)
+    dividecases(allcases,cases,partition)
+
+    recpile(args[1])
+
+    #loop of cases
+    for case in cases[1]:
+        cmdstr = "./test {inp}".format(inp = case)
+        print(cmdstr)
+        os.system(cmdstr)
+
+
+    cmdstr = "gcov {filename} --json && gunzip {filename}.gcov.json.gz".format(filename = args[1])
+    print(cmdstr)
+    os.system(cmdstr)
+
+
+    
+    
